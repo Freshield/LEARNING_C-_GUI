e8cc7b7c09ad0aeebeae8eb86b9b455cf6917a56
Freshield <zxdsw199182@gmail.com>
2015-11-18 16:07:40 -0500

    finished

--- exercise9/exercise9.cpp
+++ exercise9/exercise9.cpp
@@ -6,9 +6,88 @@
 
 using namespace std;
 
+int big(int , int );
+int sum(int a, int b);
+int cumulate(int p);
+int factorial(int p);
+int fibonacci(int p);
 
 int main()
 {
+	int a, b,x,y,z = 0;
+	cout << big(22, 11) << " is bigger" << endl;
+	cout << sum(22, 11) << endl;
+	cout << "input a,b to sum" << endl;
+	cin >> a;
+	cin >> b;
+	cout << a + b << endl;
+	cout << "input x to cumulate" << endl;
+	cin >> x;
+	cout << x << endl;
+	x = cumulate(x);
+	cout << x << endl;
+	cout << "input y to factorial" << endl;
+	cin >> y;
+	cout << factorial(y) << endl;
+	cout << "input z to fibonacci" << endl;
+	cin >> z;
+	cout << fibonacci(z) << endl;
     return 0;
 }
 
+int fibonacci(int p)
+{
+	int first = 0;
+	int second = 1;
+	int change = 0;
+
+	for (int i = 3; i <= p; i++)
+	{
+
+		change = first + second;
+		first = second;
+		second = change;
+		
+
+	}
+	return change;
+
+}
+
+int factorial(int p)
+{
+	int mul = 1;
+	for (int i = 1; i <= p; i++)
+	{
+		mul *= i;
+	}
+	return mul;
+}
+
+int cumulate(int p)
+{
+	int sum = 0;
+	for (int i = 1; i <= p; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
+
+int sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
+int big(int a, int b)
+{
+	if (a>b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
