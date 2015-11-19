b9601e3edaddcaa66f0aed8741e860f4f7a5ba7e
Freshield <zxdsw199182@gmail.com>
2015-11-18 20:21:58 -0500

    init

---
+++ exercise13/class_demo4.cpp
@@ -0,0 +1,44 @@
+#inlcude "class_demo4.h"
+
+DEMO::DEMO()
+{
+	a = 1;
+	b = 1;
+}
+
+DEMO::DEMO(int n1)
+{
+	a = n1;
+	b = n1;
+}
+
+DEMO::DEMO(int n1,int n2)
+{
+	a = n1;
+	b = n2;
+}
+
+void DEMO::set_a(int n)
+{
+	a = n;
+}
+
+void DEMO::set_b(int n)
+{
+	b = n;
+}
+
+int DEMO::get_a()
+{
+	return a;
+}
+
+int DEMO::get_b()
+{
+	return b;
+}
+
+int DEMO::dosomething()
+{
+	return a + b;
+}
---
+++ exercise13/class_demo4.h
@@ -0,0 +1,17 @@
+
+class DEMO
+{
+public:
+	DEMO();
+	DEMO(int n1);
+	DEMO(int n1, int n2);
+	void set_a(int n);
+	void set_b(int n);
+	int get_a();
+	int get_b();
+	int dosomething();
+private:
+	int a;
+	int b;
+	
+};
