f83a7ff68697c9bf496f440c6421ce4c56c42ff0
Freshield <zxdsw199182@gmail.com>
2015-11-18 19:28:58 -0500

    demo easy

--- exercise11/exercise11.cpp
+++ exercise11/exercise11.cpp
@@ -2,10 +2,53 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
+class DEMO
+{
+public:
+	void set_a(int n);
+	void set_b(int n);
+	int get_a();
+	int get_b();
+	int dosomething();
+private:
+	int a;
+	int b;
+};
+
+void DEMO::set_a(int n)
+{
+	a = n;
+}
+
+void DEMO::set_b(int n)
+{
+	b = n;
+}
+
+int DEMO::get_a()
+{
+	return a;
+}
+
+int DEMO::get_b()
+{
+	return b;
+}
+
+int DEMO::dosomething()
+{
+	return a + b;
+}
 int main()
 {
+	DEMO t;
+	t.set_a(11);
+	t.set_b(22);
+	cout << t.dosomething() << endl;
     return 0;
 }
 
